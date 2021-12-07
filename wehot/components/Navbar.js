f9bdcd235036ec564d4b51557e80c974c71335a1
CYLee <cylee820621@gmail.com>
2021-12-07 11:35:41 -0500

    re

---
+++ wehot/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h1>Wehot</h1>
+      </div>
+      <a href="">Home</a>
+      <a href="">About</a>
+    </nav>
+  );
+};
+
+export default Navbar;
