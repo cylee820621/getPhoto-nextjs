86384978a11ddf10f1fd602ff99b913519f9fa99
CYLee <cylee820621@gmail.com>
2021-12-14 17:02:03 -0500

    re

--- wehot/components/Intro.js
+++ wehot/components/Intro.js
@@ -3,8 +3,11 @@ import classes from "./Intro.module.css";
 function Intro() {
   return (
     <div className={classes.Intro}>
-      <h1>Capture your amazing life with quality photos is easy!</h1>
-      <p>We bring you an innovative way to reach out to nearby photographers in realtime, or you can make schedule on it. ​ Just one tap, and  photographer is going for you.</p>
+      <div className={classes.Content}>
+        <h1>Capture your amazing life with quality photos never been so easy!</h1>
+        <p>We bring you an innovative way to reach out to nearby photographers in realtime, or you can make schedule on it. ​ Just one tap, and  photographer is going for you.</p>
+      </div>
+      <div className={classes.Image}>Image</div>
     </div>
   );
 }
