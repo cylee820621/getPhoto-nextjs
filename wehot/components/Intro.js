7ffb31b115fe9026863ea85989cc146ddad08446
CYLee <cylee820621@gmail.com>
2021-12-14 17:15:54 -0500

    re

--- wehot/components/Intro.js
+++ wehot/components/Intro.js
@@ -2,7 +2,7 @@ import classes from "./Intro.module.css";
 
 function Intro() {
   return (
-    <div className={classes.Intro}>
+    <div className={classes.IntroContainer}>
       <div className={classes.Content}>
         <h1>Capture your amazing life with quality photos never been so easy!</h1>
         <p>We bring you an innovative way to reach out to nearby photographers in realtime, or you can make schedule on it. ​ Just one tap, and  photographer is going for you.</p>
